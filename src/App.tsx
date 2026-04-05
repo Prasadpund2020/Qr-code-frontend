bc786afb7ad98a2cb772d3c3060db1d9c4d32277
Prasad pund <118673930+Prasadpund2020@users.noreply.github.com>
2026-04-05 13:15:33 +0530

    initial commit frontend

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+// App.tsx
+import MainLayout from "@/layouts/mainlayout";
+
+function App() {
+  return <MainLayout></MainLayout>;
+}
+
+export default App;
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,14 @@
+export default function Sidebar() {
+  return (
+    <div
+      className="sidebar"
+      style={{ width: "200px", border: "1px solid #ccc", height: "100vh" }}
+    >
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
